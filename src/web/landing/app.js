72770590e9b06a4db3340bb2ef9b335746dbffa3
Jonas <stankevichus.ionas@gmail.com>
2026-05-25 12:20:20 +0500

    Connect landing to Google Form

--- src/web/landing/app.js
+++ src/web/landing/app.js
@@ -1,4 +1,5 @@
-const GOOGLE_FORM_URL = "";
+const GOOGLE_FORM_URL =
+  "https://docs.google.com/forms/d/e/1FAIpQLSf5WgpWtE3AiJXamUc3DdujkNT9a04vRexADtl3-pXUm7Cq7Q/viewform";
 
 const formLink = document.querySelector("#googleFormLink");
 const statusNode = document.querySelector("#formStatus");
@@ -7,6 +8,7 @@ if (formLink) {
   if (GOOGLE_FORM_URL) {
     formLink.href = GOOGLE_FORM_URL;
     formLink.target = "_blank";
+    formLink.removeAttribute("aria-disabled");
     statusNode.textContent = "Форма откроется в новой вкладке Google Forms.";
   } else {
     formLink.setAttribute("aria-disabled", "true");
